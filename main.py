17e080b217d5376ad8264526bd82bb98bcb34787
musabninja <87612518+musabninja@users.noreply.github.com>
2022-11-24 11:01:22 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-# This is the main file for the coursework
+import tkinter as tk
+import tkinter.font as tkFont
+
+class App:
+    def __init__(self, root):
+        #setting title
+        root.title("Simple Image Editor By Musab Al-Bahry")
+        #setting window size
+        width=862
+        height=448
+        screenwidth = root.winfo_screenwidth()
+        screenheight = root.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        root.geometry(alignstr)
+        root.resizable(width=False, height=False)
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
